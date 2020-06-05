fe9cf237f8080c22f90ba82b19cf06ee3ad959d9
Bile <bakshi.bill@gmail.com>
2020-06-05 16:03:07 +0000

    update hekko.java program

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello World! Java");  
      System.out.println("Learning Java on Youtube");
+     system.out.println("Java is little bit complex pprogramming language");
     }  
 }  
